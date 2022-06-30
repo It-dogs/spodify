86c7da6d76f477034602a2b717efa81880562f6e
YinHk <>
2022-06-30 18:33:18 -0400

    feat: add features

--- src/views/components/Menu/index.jsx
+++ src/views/components/Menu/index.jsx
@@ -10,7 +10,7 @@ import { Typography } from "@mui/material";
 //create instance for spotify web api
 const spotify = new spotifyWebApi();
 
-const categories = {topLists: 'Top Lists'};  //Global Top 50
+//const categories = {topLists: 'Top Lists'};  //Global Top 50
 
 const menuStyle = createUseStyles({
   container: {
@@ -24,7 +24,8 @@ const Menu = (props) => {
     const classes = menuStyle();
     const { token } = props;
     const [currentPage, setCurrentPage] = useState('home');
-    const [topList, setTopList] = useState([]);
+    //const [topList, setTopList] = useState([]);
+    const [categories, setCategories] = useState([]);
 
     //listen to the tab change
     useEffect(() => {
@@ -37,11 +38,22 @@ const Menu = (props) => {
       //if token exist, register that token to the api
       token && spotify.setAccessToken(token);
 
-      //call spotify web api for list of categories
+     /*  //call spotify web api for list of categories
       spotify
         .getCategories()
-          .then(data=>console.log(data))
-            .catch(err=>console.log(err));
+          .then(data=> {
+            console.log(data);
+            let list = [];
+            const categoryList = data.categoryList.categories.items;
+            categoryList.forEach(item => {
+              let temp = {};
+              temp.id = item.id? item.id:null;
+              temp.name = item.name? item.name:null;
+              !_.isEmpty(temp) && list.push(temp);
+            });
+            setCategories(list);
+          })
+            .catch(err=>console.log(err)); */
     
       return () => {
         emitter.off('home');
@@ -53,8 +65,26 @@ const Menu = (props) => {
     }, [token]);
 
     useEffect(() => {
+      
+      //call spotify web api for list of categories
       spotify
-        .getCategoryPlaylists('toplists')
+        .getCategories()
+          .then(data=> {
+            console.log(data);
+            let list = [];
+            const categoryList = data.categories.items;
+            categoryList.forEach(item => {
+              let temp = {};
+              temp.id = item.id? item.id:null;
+              temp.name = item.name? item.name:null;
+              !_.isEmpty(temp) && list.push(temp);
+            });
+            setCategories(list);
+          })
+            .catch(err=>console.log(err));
+
+      /* spotify
+        .getCategoryPlaylists('pop')
           .then(data => {
             //console.log(data.playlists.items);
             const playList = data.playlists.items;
@@ -70,14 +100,14 @@ const Menu = (props) => {
             });
             setTopList(list);
           })
-            .catch(err=>console.log(err));
+            .catch(err=>console.log(err)); */
     }, []);
 
 
     const handleMenuContent = () => <>
       <div style={{display: currentPage==='home'? 'flex':'none', height: '100%', flexDirection: 'column', justifyContent: 'start' }}>
-        <Typography sx={{color: '#FFFFFF'}}>{categories.topLists}</Typography>
-        <Home topList={topList} />
+        <Typography variant='h5' sx={{color: '#FFFFFF', paddingLeft: 5}}>{categories.topLists}</Typography>
+        <Home token={token} categories={categories} spotify={spotify}/>
       </div>
     </>
 
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Button, Card, CardContent, CardActionArea, CardActions, CardMedia, Typography } from '@mui/material';
+import { Card, CardContent, CardActionArea, CardMedia, Typography } from '@mui/material';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import useWindowSize from "../utils/hook/useWindowSize";
 import { createUseStyles } from 'react-jss';
 import { Color } from '../style/color';
+import _ from 'lodash';
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import '../style/swiperStyle.css';
 
 const homeStyle = createUseStyles({
     container: {
@@ -44,13 +50,80 @@ const homeStyle = createUseStyles({
   
 export default function Home(props) {
     const classes = homeStyle();
-    const { topList } = props;
+    const { categories, topList, spotify } = props;
     const size = useWindowSize(); 
     const [slide, setSlide] = useState(1);
+    const [playList, setPlayList] = useState([]);
 
     useEffect(() => {
-      //console.log(topList);
-    }, [topList]);
+      console.log(categories);
+      const ran = Math.floor(Math.random() * (categories.length)); console.log(ran)
+      if(ran<5) {
+        let list = [];
+        for(let i=0; i<ran; i++) {
+          //console.log(categories[i]?.id);
+          categories[i] && spotify
+            .getCategoryPlaylists(categories[i]?.id)
+              .then(data => {
+                const itemList = data.playlists.items;
+                itemList.forEach(item => {
+                  let temp = {};
+                  temp.id = item.id? item.id:null;
+                  temp.description = item.description? item.description:null;
+                  temp.url = item.images[0].url? item.images[0].url:null;
+                  temp.name = item.name? item.name:null;
+                  temp.tracks = item.tracks.href? item.tracks.href:null;
+                  !_.isEmpty(temp) && list.push(temp);
+                });
+
+               })
+               .catch(err=>console.log(err));
+        }
+        for(let j=categories.length-1; j>categories.length-6+ran; j--) {
+          //console.log(categories[j]?.id);
+          categories[j] && spotify
+            .getCategoryPlaylists(categories[j]?.id)
+              .then(data => {
+                const itemList = data.playlists.items;
+                itemList.forEach(item => {
+                  let temp = {};
+                  temp.id = item.id? item.id:null;
+                  temp.description = item.description? item.description:null;
+                  temp.url = item.images[0].url? item.images[0].url:null;
+                  temp.name = item.name? item.name:null;
+                  temp.tracks = item.tracks.href? item.tracks.href:null;
+                  !_.isEmpty(temp) && list.push(temp);
+                });
+
+               })
+               .catch(err=>console.log(err));
+        }
+        setPlayList(list);
+      } else {
+        let list = [];
+        for(let i=ran-1; i>ran-6; i--) {
+          //console.log(categories[i]?.id);
+          categories[i] && spotify
+            .getCategoryPlaylists(categories[i]?.id)
+              .then(data => {
+                const itemList = data.playlists.items;
+                itemList.forEach(item => {
+                  let temp = {};
+                  temp.id = item.id? item.id:null;
+                  temp.description = item.description? item.description:null;
+                  temp.url = item.images[0].url? item.images[0].url:null;
+                  temp.name = item.name? item.name:null;
+                  temp.tracks = item.tracks.href? item.tracks.href:null;
+                  !_.isEmpty(temp) && list.push(temp);
+                });
+
+               })
+               .catch(err=>console.log(err));
+        }
+        setPlayList(list);
+      }
+    }, [categories]);
+
     
     const slidePerView = size => {
       if(size.width>=1170 && size.width<1426) {
@@ -65,20 +138,21 @@ export default function Home(props) {
     };
 
     useEffect(() => {
-      console.log(size);
       size && slidePerView(size);
     }, [size]);
 
    
     return (
       <div className={classes.container}>
-        <Swiper
+       <Swiper
           spaceBetween={18}
           slidesPerView={slide}
-          onSlideChange={() => console.log('slide change')}
-          //onSwiper={(swiper) => console.log(swiper)}
-        >
-        { topList && topList.map( item =>
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          allowTouchMove={false}
+          navigation={true}
+          pagination={{ clickable: true }}
+        > 
+        { playList && playList.map( item =>
         <SwiperSlide>
         <Card className={classes.card}>
           <CardActionArea sx={{
@@ -102,7 +176,7 @@ export default function Home(props) {
         </Card>
         </SwiperSlide>
         )}
-        </Swiper> 
+       </Swiper>
       </div>
     ); 
 }
